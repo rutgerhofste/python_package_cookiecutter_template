9c50dd47a94dc4a2d20c9a9030b3b681ed292b62
Rutger Hofste <rutgerhofste@gmail.com>
2018-04-10 15:31:31 +0200

    first upload

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/rutgerhofste/python_package_cookiecutter_template',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
